abebf94a159c8305dd37627b9e2ff1d64411392c
Chiviguagua <74496649+Chiviguagua@users.noreply.github.com>
2024-02-15 17:39:27 -0500

    Combat ready

--- TP1-ideia/Hero.java
+++ TP1-ideia/Hero.java
@@ -4,6 +4,7 @@ public class Hero extends Characters{
     private int health = 1000;
     private int maxHealth = 10000;
     private int level = 0;
+    private double xpRequise;
     private int experience = 0;
     private int attack = 0;
 
@@ -15,6 +16,12 @@ public class Hero extends Characters{
 //   }
 
     // Getters
+    public int getLevel(){
+        return level;
+    }
+
+
+
 
     public void Heal(int n){
         health = health + n;
@@ -31,5 +38,28 @@ public class Hero extends Characters{
 
     public void Training(int n){
         attack = attack + n;
+    }
+
+    public int LevelUp(){
+        experience += 12;
+
+        xpRequise = 50 + (level + 1) * 20 * Math.pow(1.1, level + 1);
+        
+        if ( experience >= xpRequise ) {
+
+            experience = 0;
+           
+            if (level < 99) {
+
+              level++;
+              attack += 6;
+              maxHealth += 12;
+              FullHeal();
+
+            }
+        }
+        
+        return level;
+    
     }
 }
