8ed6e30cc199012ba1f1299049ac685e35985346
Gbmv <115672461+Gbmv@users.noreply.github.com>
2024-02-19 19:24:33 -0500

    Update ArgsProcessor.java
    19/02/24

--- TP1-ideia/ArgsProcessor.java
+++ TP1-ideia/ArgsProcessor.java
@@ -8,12 +8,12 @@ public class ArgsProcessor {
         char firstLetter = phrase[0].charAt(0);
         System.out.println(firstLetter); // debugger first letter
 
-        
 
-        
+
+
         // Hero health points
         int health = Integer.parseInt(phrase[1]);
-        //System.out.println("SET: " + health);
+
         // Hero attack points
         int attack = Integer.parseInt(phrase[2]);
 
@@ -27,30 +27,64 @@ public class ArgsProcessor {
         } else {
             hero = new Hero(health, attack);
         }
-        
-        
-        
-        //Hero hero = new Hero(health, attack);
-        Enemy enemy = new Enemy();
-        
-        //Enemy enemy = new Enemy(100, 35, 25);
 
-        // Concertar
-        for (int i = 0; i < phrase.length; i++) {
+
+
+
+        Enemy enemy = new Enemy();
+
+
+
+
+        for (int i = 0; i < phrase.length  ; i++) {
             // System.out.println(phrase[i] + "");
             // System.out.println("------------------------------");
 
+            // Looping to read all the messages
             if (doAction(phrase[i], hero, enemy) == false) {
-                System.out.println("died after beating " + Combat.enemiesDefeat + " enemies and attaining level " + hero.getLevel() + "!");
+                System.out.println( "In his quest " + phrase[0]  +" died after beating " + Combat.enemiesDefeat +
+                        " enemies and attaining level " + hero.getLevel() + "!");
                 return;
             }
-            
+
 
             // this loop must pick all the do allctions and give a full message in the end.
         }
-        System.out.println("succes :D");
+        System.out.println("In his quest," + phrase[0] + " beat  " + Combat.enemiesDefeat + " enemies , attained level "
+                + hero.getLevel() + " and survived with " + hero.getAttack() + "HP" );
 
     }
+
+
+    //         Une façon de verifier si a une case fought, mais ne donne pas son valeur correct au final
+
+
+    //        boolean anyFoughtAction = false;
+//
+//        for (String action : phrase) {
+//            if (action.startsWith("fought")) {
+//                anyFoughtAction = true;
+//            } else {
+//                if (!doAction(action, hero, enemy)) {
+//                    System.out.println("In his quest " + phrase[0] + " died after beating " + Combat.enemiesDefeat +
+//                            " enemies and attaining level " + hero.getLevel() + "!");
+//                    return;
+//                }
+//            }
+//        }
+//
+//        // If any "fought" action was found, process them now
+//        if (anyFoughtAction) {
+//            for (String action : phrase) {
+//                if (action.startsWith("fought")) {
+//                    if (!doAction(action, hero, enemy)) {
+//                        System.out.println("In his quest " + phrase[0] + " died after beating " + Combat.enemiesDefeat +
+//                                " enemies and attaining level " + hero.getLevel() + "!");
+//                        return;
+//                    }
+//                }
+//            }
+//        }
 
     private static String[] makePhrase(String args) {
         return args.trim().split(",");
@@ -60,8 +94,8 @@ public class ArgsProcessor {
     // puis effectue l'action correspondante
     // retourne true si le joueur survit à l'action, false sinon
     private static boolean doAction(String action, Hero hero, Enemy enemy) {
-        
-        
+
+
         // ici, on transforme le String action en un tableau de String, en séparant les
         // mots par des espaces
         String[] phrase = action.trim().split(" ");
@@ -70,7 +104,7 @@ public class ArgsProcessor {
         System.out.println("phrase:  " + phrase);
         System.out.println("------------------------------");
 
-        
+
 
 
         switch (phrase[0]) {
@@ -78,27 +112,19 @@ public class ArgsProcessor {
             case "fought":
                 Combat combat = new Combat(hero, enemy);
                 return combat.fight(phrase);
-                //System.out.println(combat.fight(phrase));
-                // TODO : Handle the fight
-
             case "rested":
                 hero.FullHeal();
                 System.out.println("hero status:   FULL_HEAL");
-                // TODO : Handle the resting
                 break;
             case "healed":
                 int healthAdded = Integer.parseInt(phrase[1]);
-                // Change for give an message
                 hero.Heal(healthAdded);
                 System.out.println("hero status: HEALED " + healthAdded + " HP!");
-                // TODO : Handle the healing
                 break;
             case "trained":
                 int attackAdded = Integer.parseInt(phrase[3]);
                 hero.Training(attackAdded);
                 System.out.println("hero status: TRAINED to get" + attackAdded + " Att!");
-
-                // TODO : Handle the training
                 break;
         }
         return true;
