2bf63609e9968c3b2f601bd33a000d56c1961cf9
Gbmv <115672461+Gbmv@users.noreply.github.com>
2024-02-19 19:21:59 -0500

    Update Combat.java

--- TP1-ideia/Combat.java
+++ TP1-ideia/Combat.java
@@ -1,31 +1,19 @@
 public class Combat {
-    
-   private Hero hero;
-   private Enemy enemy;
 
-  public Combat(Hero hero, Enemy enemy) {
-    this.hero = hero;
-    this.enemy = enemy;
-  }
+    private Hero hero;
+    private Enemy enemy;
 
-    
-  //int heroAttack = hero.getAttack();
-  //int enemyAttack = enemy.getAttack();
-  //int enemiesDefeat = 0; // counter of enemies defeted
+    public Combat(Hero hero, Enemy enemy) {
+        this.hero = hero;
+        this.enemy = enemy;
+    }
 
-    // Output
-    String output;
+
+
     static int enemiesDefeat;
-    
+
 
     public boolean fight(String[] phrase) {
-
-
-        //int heroAttack = hero.getAttack();
-        //int enemyAttack = enemy.getAttack();
-        //int enemiesDefeat = 0; // counter of enemies defeted
-
-
 
         int numberEnemies = Integer.parseInt(phrase[1]);
         System.out.println("pre While 1 ");
@@ -33,17 +21,19 @@ public class Combat {
         System.out.println("Number enemies: " + numberEnemies);
 
         for (int i=0; i < numberEnemies; i++   ){
-        //while (hero.getHealth() > 0 && numberEnemies > 0) {
+            //while (hero.getHealth() > 0 && numberEnemies > 0) {
             System.out.println("While 1");
 
             //int damageToEnemy = heroAttack;
-            System.out.println(hero.getAttack());
+            System.out.println( "Hero attack = " + hero.getAttack());
             //int damageToHero = enemyAttack;
-            System.out.println(enemy.getAttack());
+            System.out.println("Enemy atack = " + enemy.getAttack());
 
 
             while (hero.getHealth() > 0) {
                 System.out.println("While 2 ");
+
+
 
                 // Calcule of the damage to the enemy
                 int newEnemyHealth = enemy.getHealth() - hero.getAttack();
@@ -93,20 +83,15 @@ public class Combat {
                 }
                 System.out.println("inside");
             }
+
             System.out.println("outside");
 
         }
-
         if (hero.getHealth() <= 0) {
-            output = "died after beating " + enemiesDefeat + " enemies and attaining level " + hero.getLevel() + "!"; // TODO:
             return false;                                                                                                          // did
-                                                                                                                      // he
-                                                                                                                      // Lv.
-                                                                                                                      // UP?
+
         } else {
-            output = "beat " + enemiesDefeat + " enemies,";
             return true;
         }
-
-        }
+    }
 }
